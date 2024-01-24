053b76f7b8927d76d94bee5ee85d3d90c03978b5
Nicky Hama <njhama@usc.edu>
2024-01-23 22:55:05 -0800

    ch2

--- script.py
+++ script.py
@@ -1,51 +1,133 @@
 import os
 
-# Directory where the images are located
-directory_path = "answers/chapter1/Self-Test"
+directory_path = "answers/chapter2/Problems"
 
-# List of new names
 new_names = [
-    "1",
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-    "10",
-    "11",
-    "12",
-    "13",
+    "01",
+    "02",
+    "03",
+    "04a",
+    "04b",
+    "05a",
+    "05b",
+    "05c",
+    "05d",
+    "06a",
+    "06b",
+    "06c",
+    "06d",
+    "07a",
+    "07b",
+    "07c",
+    "08a",
+    "08b",
+    "08c",
+    "09",
+    "10a",
+    "10b",
+    "11a",
+    "11b",
+    "12a",
+    "12b",
+    "12c",
+    "13a",
+    "13b",
+    "13c",
+    "13d",
+    "13e",
     "14",
-    "15",
-    "16",
+    "15a",
+    "15b",
+    "15c",
+    "15d",
+    "15e",
+    "16a",
+    "16b",
+    "16c",
+    "16d",
+    "16e",
+    "16f",
+    "16g",
     "17",
     "18",
-    "19"
+    "19",
+    "20",
+    "21a",
+    "21b",
+    "22",
+    "23",
+    "24",
+    "25",
+    "26",
+    "27",
+    "28a",
+    "28b",
+    "28c",
+    "29aa",
+    "29ab",
+    "29ac",
+    "29b",
+    "29c",
+    "30a",
+    "30b",
+    "30c",
+    "31a",
+    "31b",
+    "32",
+    "33",
+    "34",
+    "35a",
+    "35b",
+    "35c",
+    "35d",
+    "36a",
+    "36b",
+    "37a",
+    "37b",
+    "38",
+    "39",
+    "40",
+    "41",
+    "42",
+    "43a",
+    "43b",
+    "44a",
+    "44b",
+    "44c",
+    "45a",
+    "45b",
+    "46",
+    "47",
+    "48",
+    "49",
+    "50",
+    "51",
+    "52a",
+    "52b",
+    "53",
+    "54",
+    "55a",
+    "55b",
+    "56"
 ]
 
-# Get the list of image files in the directory
 image_files = [f for f in os.listdir(directory_path) if os.path.isfile(os.path.join(directory_path, f))]
 
-# Sort the image files alphabetically
 image_files.sort()
 
-# Rename the image files with the new names
 for i, image_file in enumerate(image_files):
     if i < len(new_names):
-        # Get the file extension
+       
         file_extension = os.path.splitext(image_file)[1]
-        # Construct the new file name with the new name and file extension
+  
         new_file_name = f"{new_names[i]}{file_extension}"
-        # Full path of the original and new files
+   
         old_file_path = os.path.join(directory_path, image_file)
         new_file_path = os.path.join(directory_path, new_file_name)
-        # Check if the file already exists and remove it
+        
         if os.path.exists(new_file_path):
             os.remove(new_file_path)
-        # Rename the file
+     
         os.rename(old_file_path, new_file_path)
         print(f"Renamed: {image_file} -> {new_file_name}")
 
